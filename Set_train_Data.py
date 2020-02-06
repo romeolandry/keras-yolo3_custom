ee58e012026125e2c634893c876cc04b50844046
romeolandry <kamgoche@yahoo.com>
2020-02-06 15:12:38 +0100

    create configuration folder

---
+++ Set_train_Data.py
@@ -0,0 +1,5 @@
+""" for the custom model we will use data from Dataset V5 
+"""
+# read given data from DatasetV5 and save It if not exit on Disk
+# Convert the downlowded data into Xml File
+# Convert in to Yolo3 readable file 
