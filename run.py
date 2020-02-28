71803c8aad72698eb1ed16483e0096680ba4eda1
romeolandry <kamgoche@yahoo.com>
2020-02-28 16:20:35 +0100

    Update run.py and condition to convert model

--- run.py
+++ run.py
@@ -7,15 +7,22 @@ import modules.parser as pars
 import download_data as dd
 import modules.convert as conv_to_keras
 import train as tr
+import yolo_video as test
 
 if __name__ == "__main__":
     args = pars.parser_arguments()
     
     if args.option == 'train':
         print("train option was choosed")
-        conv_to_keras.run_convertor(args)
+        if args.convert:
+            print(" convert bevore")
+            conv_to_keras.run_convertor(args)
         tr.train()
     if args.option == 'download':
         dd.downloader(args)
-    if args.option == 'export':
-        print("export model to ONNX")
+    if args.option == 'test':
+        if args.convert:
+            print(" convert bevore")
+            conv_to_keras.run_convertor(args)
+        test.test_yolo3(args)
+        print("test yolo model")
