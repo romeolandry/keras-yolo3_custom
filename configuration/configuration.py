e65d418c88cb38f01094b88f2ede95407c312526
romeolandry <kamgoche@yahoo.com>
2020-02-28 13:06:13 +0100

    download verified

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -9,8 +9,8 @@ Name_dataset = "Custom_data" # Name of your folder Dataset
 classe = ['Canoe'] # Witch Classes will be Downloaded to see availabel classe ['Apple','Orange']
 type_of_Data = 'train' # witch dataset did you wont to Downloaded e.g:['train', 'test', 'validation', 'all']
 Quantty = 4 # How many File did you wont
-class_name =  'Class_4_'+ Name_dataset +'.txt'
-class_file_classes= class_name +'_classes.txt'
+annotation_file =  'Class_4_'+ Name_dataset +'.txt'
+class_file_classes= annotation_file +'_classes.txt'
 
 """
     Confguration to convert Darknet To Keras to keras model
