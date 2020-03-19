6757fe22991ccbd7bd50bb836ed090e63608fe9f
kamgo romeo <kamgo@kamgo-ThinkPad-T430>
2020-03-19 14:17:44 +0100

    .gitignore update add csv

---
+++ modules/save_param.py
@@ -0,0 +1,12 @@
+"""
+
+save in to a csv-File all needed parameter thal will deeing use to train a model 
+and also the accuracy of model
+
+"""
+import os 
+import io
+from configuration import configuration as config_data
+
+del save_to_csv ():
+    print("save")
