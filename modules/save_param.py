0a8fc60a66a27f2a035d1366e768a64a7a082bfe
kamgo romeo <kamgo@kamgo-ThinkPad-T430>
2020-03-19 14:18:19 +0100

    save_param.py add for save parameter for training

--- modules/save_param.py
+++ modules/save_param.py
@@ -4,9 +4,21 @@ save in to a csv-File all needed parameter thal will deeing use to train a model
 and also the accuracy of model
 
 """
-import os 
+import os
 import io
-from configuration import configuration as config_data
+import pandas as pd
+from configuration import configuration as data_config
 
-del save_to_csv ():
-    print("save")
+
+def to_csv(Last_Model,annotations,file_classe,Classes_number, anchors, log_dir):
+    
+    data = [{'Last Model':Last_Model,'annotations': annotations,'file_classe':file_classe,'Classes_number': Classes_number, 'anchors':anchors, 'log_dir':log_dir}]     
+    df = pd.DataFrame(data, columns=['Last Model','annotations','file_classe','Classes_number', 'anchors', 'log_dir']) 
+    if not os.path.exists(data_config.csv_path):
+        try:
+            df.to_csv(data_config.csv_path, mode='a', header=True)
+        except OSError as exc:
+            #if exc.errno != errno.ENOENT:
+            raise
+    else:
+        df.to_csv(data_config.csv_path, mode='a', header=False)
